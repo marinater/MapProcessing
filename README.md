4063a73164052d33769cff2046b39e00cadc5918
Samarth Patel <samarthxpatel@gmail.com>
2019-11-14 19:50:59 -0500

    Cleaned some things up

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# MapProcessing
- Automatic HPA* Pipeline for Satellite Imagery
+# Map Processessing
+Automatic HPA* Pipeline for Satellite Imagery
+## Map Tile-Set Processing Pipeline
+ - To use HPA*, we need to create multiple, in this case 2, hierarchies of graphs. The first is a set of intra-tile graphs and the second is a large inter-tile graph. The inter-tile graph is used to find the general route from point A to point B across tiles. The intra-tile graphs are then used to pick out the fastest route for each tile along that general route. The benefit of this is that it scales very well and should work on extremely large tile-sets.
+
+To create an intra-tile graph:
+ 1. Load image and thin all roads to a uniform 1 pixel thickness
+ 2. Identify all intersections and endpoints as nodes
+ 3. Uniquely label connected components
+ 4. Connect all nodes with same label
+
+To create inter-tile graph:
+ 1. Create an empty graph to store everything
+ 2. Load and merge all tiles in a column
+	 1. Create an empty graph
+	 2. Load a simplified intra-tile graph with only tile entrances and exits
+	 3. Merge new graph with the combined column graph
+	 4. Repeat
+ 3. Merge column graph with the combined graph
+ 4. Repeat for all columns
+
+Merging graphs is done by keeping track of the nodes along the leading edges of the combined graph. When a new tile is being merged, overlapping nodes on the leading edge are combined. Connections to the deleted node are then updated to reflect the new node_id.
+
+multi_tile_processor.py is used to create the combined graph and visualization for testing
+tile_processor.py has Class definitions and creates the inter-tile graphs
+
+## TO DO
+Implement HPA* on the generated graphs
--- multi_tile_processor.py
+++ multi_tile_processor.py
@@ -11,38 +11,40 @@ from tqdm import tqdm
 # start_x, start_y = (0, 0)
 # end_x, end_y = (1, 1)
 
-# filetype = 'jpg'
-# source = './roads'
-# tile_size = (512, 512)
-# start_x, start_y = (7779, 12517)
-# end_x, end_y = (7781, 12616)
-
 filetype = 'jpg'
 source = './roads'
 tile_size = (512, 512)
-start_x, start_y = (7779, 12612)
-end_x, end_y = (7781, 12616)
+start_x, start_y = (7779, 12517)
+end_x, end_y = (7786, 12527)
+
+# filetype = 'jpg'
+# source = './roads'
+# tile_size = (512, 512)
+# start_x, start_y = (7779, 12612)
+# end_x, end_y = (7781, 12616)
 
 def stitchTiles():
+	'''Stitch tiles in the input range for visualization purposes'''
+
 	total_size = (end_y - start_y + 1) * tile_size[0], (end_x - start_x + 1) * tile_size[1], 3
 	out = np.zeros( total_size, dtype=np.uint8)
 
 	for x in range(end_x - start_x + 1):
-		if x > 0:
-			out[ :, x * tile_size[1], 2 ] = 255
-	
 		for y in range(end_y - start_y + 1):
 			filename = '{}/{}_{}.{}'.format(source,x + start_x,y + start_y,filetype)
 			y_range = tile_size[0] * y, tile_size[0] * y + tile_size[0]
 			x_range = tile_size[1] * x, tile_size[1] * x + tile_size[1]
 
-			out[ slice(*y_range), slice(*x_range), 0] = cv2.imread(filename, cv2.IMREAD_GRAYSCALE)
+			out[ slice(*y_range), slice(*x_range), :] = cv2.imread(filename)
 			if y > 0:
-				out[ y * tile_size[0], :, 2 ] = 255
-		
+				out[ y * tile_size[0], :, : ] = 0, 0, 255
+		if x > 0:
+			out[ :, x * tile_size[1], : ] = 0, 0, 255
+
 	return out
 
 def graph2img(graph):
+	'''Create an image from the graph visualization purposes'''
 	graph = graph.boundary_graph
 	total_size = (end_y - start_y + 1) * tile_size[0], (end_x - start_x + 1) * tile_size[1]
 	out = np.zeros( total_size, dtype=np.uint8)
@@ -53,15 +55,13 @@ def graph2img(graph):
 
 		for connected_node in node.connected:
 			pt2 = graph[connected_node].position
-			if (pt1[0] - pt2[0])**2 + (pt1[0] - pt2[0])**2 > 524288:
+			if (pt1[0] - pt2[0])**2 + (pt1[0] - pt2[0])**2 < 524288:
 				out = cv2.line(out, pt1[::-1], pt2[::-1], node_color, 1, 8)
 
 	disp = cv2.applyColorMap(out, cv2.COLORMAP_HSV)
 	disp[out == 0, 2] = 0
 
 	for node in graph.values():
-		# if np.array_equal(disp[ node.position[::-1]], white):
-		# 	print('err')
 		cv2.circle(disp, node.position[::-1], 3, (255,255,255), 5)
 
 	for node in graph.values():
@@ -70,9 +70,16 @@ def graph2img(graph):
 	return disp
 
 def map2new(old, new):
+	'''
+	Identifies matching nodes along two tile boundaries.
+	Currently just maps the nodes in the order they came
+	'''
 	return { new[i] : old[i] for i in range(min(len(new),len(old))) }
 
 def replaceNodes(tile, leading_edge, new_edge):
+	'''
+	Finds old node ids with the corresponding new node id
+	'''
 	conversion_map = map2new(leading_edge, new_edge)
 
 	# Replace all instances of the old with the new
@@ -93,6 +100,7 @@ def mergeGraphs(combined, new):
 			combined.boundary_graph[key] = new.boundary_graph[key]
 
 def checkGraph(graph):
+	'''Checks that all connections are bidirectional'''
 	graph = graph.boundary_graph
 	for node_id in graph.keys():
 		for connected_node_id in graph[node_id].connected:
@@ -102,6 +110,7 @@ def checkGraph(graph):
 				return False
 	return True
 
+# Explanation of process in readme
 combined_graph = Graph()
 for x in tqdm(range(start_x, end_x + 1)):
 	combined_col_graph = Graph()
@@ -130,9 +139,13 @@ for x in tqdm(range(start_x, end_x + 1)):
 	mergeGraphs(combined_graph, combined_col_graph)
 	combined_graph.boundaries['east'] = combined_col_graph.boundaries['east']
 
-print(checkGraph(combined_graph))
-img = graph2img(combined_graph)
+if checkGraph(combined_graph):
+	print('Graph is valid')
+else:
+	print('Invalid graph! At least 1 connection is not bidirectional!!!!!')
+
+graph_img = graph2img(combined_graph)
 stitched = stitchTiles()
 
-cv2.addWeighted(img, .5, stitched, 1, 0, stitched)
-cv2.imwrite('out.png', stitched)
+cv2.addWeighted(graph_img, 1, stitched, 0.5, 0, stitched)
+cv2.imwrite('visualization.png', stitched)
